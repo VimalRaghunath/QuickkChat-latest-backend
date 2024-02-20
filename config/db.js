a7af64c45e4e1d543a4886216300f659c8c6780b
VimalRaghunath <vimalvrk@gmail.com>
2024-02-20 10:09:18 +0530

    daily updation backend

--- config/db.js
+++ config/db.js
@@ -3,17 +3,16 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-     const conn = await mongoose.connect(process.env.MONGO, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-     });
+    const conn = await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-     console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.bold);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.bold);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit();
   }
-}
-
+};
 
 module.exports = connectDB;
