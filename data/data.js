95d3cf5fac207ac40b3538be8f3e0e48e000e90c
VimalRaghunath <vimalvrk@gmail.com>
2024-02-21 09:47:29 +0530

    daily updation

--- data/data.js
+++ data/data.js
@@ -1,30 +1,29 @@
 const chats = [
-    {
-        isGroupchat: false,
-        users: [
-            {
-                name:" George",
-                email:"george@gmail.com"
-            },
-            {
-                name:"Arun",
-                email:"arun@gmail.com"
-            },
-        ],
+  {
+    isGroupchat: false,
+    users: [
+      {
+        name: " George",
+        email: "george@gmail.com",
+      },
+      {
+        name: "Arun",
+        email: "arun@gmail.com",
+      },
+    ],
 
-        _id: "123456",
-        chatname:"george"
-        
-    },
-    {
-        isGroupchat: false,
-        users: [
-            {
-                name:"Guest user",
-                email:"eorge@gmail.com"
-            }
-        ]
-    }
-]
+    _id: "123456",
+    chatname: "george",
+  },
+  {
+    isGroupchat: false,
+    users: [
+      {
+        name: "Guest user",
+        email: "eorge@gmail.com",
+      },
+    ],
+  },
+];
 
 module.exports = { chats };
