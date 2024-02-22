b24e6099285d6614f304ac842ea7add9cf0cd541
VimalRaghunath <vimalvrk@gmail.com>
2024-02-22 09:49:37 +0530

    daily updation

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const chats = require('./data/data')
+const chats = require("./data/data");
 const dotenv = require("dotenv");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 const colors = require("colors");
 
 dotenv.config();
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
 
-app.get('/', (req,res) => {
-    res.send("Api is running")
-})
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
-app.get('/api/chat', (req,res) => {
-    res.send(chats);
-})
-
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 7000;
 
 app.listen(7000, console.log(`Server Running on PORT ${PORT}`.yellow.bold));
