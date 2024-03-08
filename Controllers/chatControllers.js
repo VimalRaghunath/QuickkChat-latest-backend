dee90db1efefeb5c1e104cde43f2610f6f46e34e
VimalRaghunath <vimalvrk@gmail.com>
2024-03-09 00:33:59 +0530

    accesschat completed

--- Controllers/chatControllers.js
+++ Controllers/chatControllers.js
@@ -1,5 +1,19 @@
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const Chat = require("../Model/ChatSchema");
 
-const accessChat = asyncHandler( async (req, res) => {
-    const { userId } = req.body;
+const accessChat = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    console.log("userId param not sent with request");
+    return res.sendStatus(400);
+  }
+
+  var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  });
 });
