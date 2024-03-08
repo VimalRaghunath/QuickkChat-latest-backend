cfdb73a55e43e9a6a1e20cd89be276c00e84adea
VimalRaghunath <vimalvrk@gmail.com>
2024-03-09 00:03:04 +0530

    chat user routes created

---
+++ Controllers/chatControllers.js
@@ -0,0 +1,5 @@
+const asyncHandler = require("express-async-handler")
+
+const accessChat = asyncHandler( async (req, res) => {
+    const { userId } = req.body;
+});
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { registeruser, authUser, allUsers } = require('../Controllers/userControllers');
+const { protect } = require('../Middlewares/authMiddleware');
 
 
 const router = express.Router()
 
-router.route('/').post(registeruser).get(allUsers);
+router.route('/').post(registeruser).get(protect, allUsers);
 router.post('/login', authUser);
 
 
