e1e90c9ef0141e4fcfadafe77460d0e02b650a7a
VimalRaghunath <vimalvrk@gmail.com>
2024-03-17 18:31:58 +0530

    sendMessage and fetching all messages are completed

---
+++ Routes/messageRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { protect } = require("../Middlewares/authMiddleware");
+const { sendMessage, allMessages } = require("../Controllers/messageControllers");
+
+
+const router = express.Router()
+
+
+router.route("/").post(protect, sendMessage)
+router.route("/:chatId").get(protect, allMessages)
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./Routes/userRoutes")
 const chatRoutes = require("./Routes/chatRoutes")
+const messageRoutes = require("./Routes/messageRoutes")
 const { notFound, errorHandler } = require("./Middlewares/errorHandlingMiddlewares")
 const cors = require('cors');
 
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
